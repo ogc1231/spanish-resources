835de78ce6047770988070b48301d2758d52948d
ogc1231 <ogc@live.ie>
2023-08-30 10:31:48 +0000

    Add delete post functionality

--- posts/models.py
+++ posts/models.py
@@ -3,12 +3,39 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
+    image_filter_choices = [
+        ('argentina', 'Argentina'),
+        ('bolivia', 'Bolivia'),
+        ('chile', 'Chile'),
+        ('colombia', 'Colombia'),
+        ('costa_rica', 'Costa Rica'),
+        ('cuba', 'Cuba'),
+        ('dominican_republic', 'Dominican Republic'),
+        ('ecuador', 'Ecuador'),
+        ('el_salvador', 'El Salvador'),
+        ('equatorial_guinea', 'Equatorial Guinea'),
+        ('guatemala', 'Guatemala'),
+        ('honduras', 'Honduras'),
+        ('mexico', 'Mexico'),
+        ('nicaragua', 'Nicaragua'),
+        ('panama', 'Panama'),
+        ('paraguay', 'Paraguay'),
+        ('peru', 'Peru'),
+        ('spain', 'Spain'),
+        ('uruguay', 'Uruguay'),
+        ('venezuela', 'Venezuela'),
+    ]
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
-    image = models.ImageField(upload_to='images/', default='../default_post_cvimec', blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_post_cvimec', blank=True
+    )
+    image_filter = models.CharField(
+        max_length=32, choices=image_filter_choices, default='normal'
+    )
 
     class Meta:
         ordering = ['-created_at']
