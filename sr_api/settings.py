22e9243be0bbc06072c30dff409c625681c29e30
ogc1231 <ogc@live.ie>
2023-09-01 09:44:29 +0000

    Update client_origin

--- sr_api/settings.py
+++ sr_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -40,8 +41,9 @@ DEBUG = 'DEV' in os.environ
 
 # ALLOWED_HOSTS = ['8000-ogc1231-spanishresource-okwc2hijqh5.ws-eu104.gitpod.io']
 
-ALLOWED_HOSTS = ['spanish-resources-5768efbafb33.herokuapp.com', 'localhost', '8000-ogc1231-spanishresource-okwc2hijqh5.ws-eu104.gitpod.io']
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'), 'localhost', '8000-ogc1231-spanishresource-okwc2hijqh5.ws-eu104.gitpod.io']
 
+CLIENT_ORIGIN_DEV = [os.environ.get('CLIENT_ORIGIN')]
 
 # Application definition
 
@@ -114,9 +116,10 @@ if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
     ]
-else:
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
